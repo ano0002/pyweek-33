37b5a0648d00e49c564c3073b080c6c66ff3364f
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2022-03-24 23:14:03 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+from turtle import position
 from ursina import *
 
 class CustomButton(Entity):
@@ -28,6 +29,47 @@ class CustomButton(Entity):
     def on_click(self):
         if self.disabled:
             return
+        
+class Bubble(Text):
+    
+    def __init__(self,text,position,next=None, **kwargs):
+        super().__init__(text=text[:1],collider="box",current_color =color.black)
+        if isinstance(position,Entity):
+            self.position = position.position + Vec3(position.scale_x/2,position.scale_y/2,0)
+        else :
+            self.position = position
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.full_text=text
+        self.create_background()
+        self.appear()
+
+
+    def on_click(self):
+        if self.appear_sequence:
+            self.appear_sequence.finish()
+        
+    def appear(self,text=None, speed=.025):
+        from ursina.ursinastuff import invoke
+        self.enabled = True
+        if self.appear_sequence:
+            self.appear_sequence.finish()
+
+        target_text = text if text != None else self.full_text
+        self.full_text = target_text
+        self.appear_sequence = Sequence()
+        tn = self.text_nodes[0]
+        tn.node().setText('')
+        new_text = ''
+
+        for char in target_text:
+            new_text += char
+            self.appear_sequence.append(Wait(speed))
+            self.appear_sequence.append(Func(tn.node().setText, new_text))
+            self.appear_sequence.append(Func(setattr,self.background,"scale_x",self.scale_x))
+
+        self.appear_sequence.start()
+        return self.appear_sequence
 
 
 
@@ -35,5 +77,9 @@ if __name__ == "__main__":
     app = Ursina()
     window.color = color.rgb(11,11,11)
     scene.fog_density = 0
-    CustomButton("play_button",scale = 0.2,on_click=Func(print,"hey"))
+    def new_text():
+        Bubble("play_button sometimes again",position=(0,.2))
+    play = CustomButton("play_button",scale = 0.2,on_click=new_text)
+    
+    _ed = EditorCamera()
     app.run()
